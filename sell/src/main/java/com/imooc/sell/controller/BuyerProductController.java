eaf556a723d3eaf2dfeb2aed4cbc976a48f1ded1
k <511158469@qq.com>
2020-02-06 16:38:53 +0800

    update sell

---
+++ sell/src/main/java/com/imooc/sell/controller/BuyerProductController.java
@@ -0,0 +1,40 @@
+package com.imooc.sell.controller;
+
+import com.imooc.sell.VO.ResultVO;
+import com.imooc.sell.dataobject.ProductInfo;
+import com.imooc.sell.service.CategoryService;
+import com.imooc.sell.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * 买家商品
+ * Created by 廖师兄
+ * 2017-05-12 14:08
+ */
+@RestController
+@RequestMapping("/buyer/product")
+public class BuyerProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping("/list")
+    public ResultVO list() {
+        //1. 查询所有的上架商品
+        List<ProductInfo> productInfoList = productService.findUpAll();
+
+        //2. 查询类目(一次性查询)
+
+        //3. 数据拼装
+
+       return null;
+    }
+}
